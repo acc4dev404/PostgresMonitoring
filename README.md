1a73fdb31758d2ed5161933fcf3c85039c66c32a
Artem Valeev <acc4dev404@gmail.com>
2024-09-09 01:26:38 +0300

    Create docker project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Postgres monitoring
+
+IP: `192.168.1.45`
+
+Run `docker compose up`
+
+Run `docker compose up -d`
+
+Run `docker-compose up --build -d`
+
+View logs `docker compose logs -f`
---
+++ docker/postgres/postgres-migrations/migrations.sql
@@ -0,0 +1,44 @@
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+
+CREATE TABLE IF NOT EXISTS "users" (
+    -- "login" VARCHAR(50) PRIMARY KEY,
+    "login" VARCHAR(50) UNIQUE NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "date" TIMESTAMP DEFAULT Now()
+);
+
+CREATE TABLE IF NOT EXISTS "emails" (
+    -- "login" VARCHAR(50) PRIMARY KEY,
+    -- "login" VARCHAR(50) UNIQUE NOT NULL,
+    -- "email" VARCHAR(255) UNIQUE NOT NULL,
+    "login" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    FOREIGN KEY ("login") REFERENCES "users" ("login")
+);
+
+-- UNIQUE
+CREATE INDEX users_login_idx_btree ON "users" ("login");
+CREATE INDEX emails_email_idx_btree ON "emails" ("email");
+
+-- INSERT INTO "users" ("login", "password")
+-- VALUES ('Test 1', 'Test 1'),
+--        ('Test 2', 'Test 2'),
+--        ('Test 3', 'Test 3'),
+--        ('Test 4', 'Test 4'),
+--        ('Test 5', 'Test 5');
+
+
+-- INSERT INTO "emails" ("login", "email")
+-- VALUES ('Test 1', 'Test1@mail.ru'),
+--        ('Test 2', 'Test2@mail.ru'),
+--        ('Test 3', 'Test3@mail.ru'),
+--        ('Test 4', 'Test4@mail.ru'),
+--        ('Test 5', 'Test5@mail.ru');
+
+
+-- SELECT calls, total_exec_time, query FROM pg_stat_statements ORDER BY calls DESC LIMIT 10;
+
+-- EXPLAIN SELECT * FROM users WHERE login LIKE 'Test 1000';
+-- SELECT * FROM "users" WHERE "login" LIKE 'Test 1000';
+
+-- SELECT * FROM "users" JOIN "emails" ON "users"."login" = "emails"."login" WHERE "users"."login" LIKE '%9%';
